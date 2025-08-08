0e22279320a8ca8953ad0422833616c1599f5663
David S <raphdavids21@gmail.com>
2025-08-08 09:17:14 +0700

    Update 01_basic_calculator.cpp

--- 01_basic_calculator.cpp
+++ 01_basic_calculator.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
-  int x, y;
+  int x, y, z;
   char op;
   cout << "Please enter X and Y" << endl;
   cin >> x >> y;
-  cout << "Enter operator (+, -, *, %): " << endl;
+  cout << "Enter operator (+, -, *, /): " << endl;
   cin >> op;
   if(op == '+'){
       cout << x + y << endl;
@@ -16,8 +17,9 @@ int main(){
   else if(op == '*'){
       cout << x * y << endl;
   }
-  else if(op == '%'){
-      cout << x % y << endl;
+  else if(op == '/'){
+      z = round(x/double(y));
+      cout << z << endl;
   }
   else{ cout << "Invalid operator";}
   return 0;
